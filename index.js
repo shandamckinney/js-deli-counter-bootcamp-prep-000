efc9cbdfaacdafe8751209048ab572321e3730d5
shandamckinney <mckinney.shanda@gmail.com>
2018-05-11 23:03:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ function nowServing(katzDeli) {
   return (`Currently serving ${katzDeli.shift()}.`);
 }
 
-var line = []
-function currentLine(line){
-  for(let i=0; i< katzDeli.length; i++){
-    line.push(``+[i+i]+`.` + katzDeli[i])
-  }
-  if (katzDeli.length === 0){
-    return "The line is currently empty."
-  } else
-    return (`The line is currently:` + line)
+var line = [];
 
+function currentLine(katzDeli) {
+  for (let i = 0; i < katzDeli.length; i++) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
 }
