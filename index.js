0e91f0123d1c3c63cf5d20949e747d3ee1563ddd
shandamckinney <mckinney.shanda@gmail.com>
2018-05-11 23:02:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,14 @@ function nowServing(katzDeli) {
   else
   return (`Currently serving ${katzDeli.shift()}.`);
 }
+
+function currentLine(line){
+  for(let i=0; i< katzDeli.length; i++){
+    line.push(``+[i+i]+`.` + katzDeli[i])
+  }
+  if (katzDeli.length === 0){
+    return "The line is currently empty."
+  } else 
+    return (`The line is currently:` + line)
+  
+}
