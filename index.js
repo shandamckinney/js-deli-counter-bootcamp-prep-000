5b81c82a57c13dc36b99db8f5f0b59056e9b34e6
shandamckinney <mckinney.shanda@gmail.com>
2018-05-11 22:57:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function takeANumber(katzDeli, name){
   katzDeli.push(`${name}`)
   return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`)
 }
+
+function nowServing(){
+  let line = 0
+  while (line < katzDeli.length){
+    i++;
+  }
+  if (katzDeli.length === 0){
+    return "There is nobody waiting to be served!"
+  }
+  else {
+    return (`Currently serving ${katzDeli.shift()}`)
+  }
+}
